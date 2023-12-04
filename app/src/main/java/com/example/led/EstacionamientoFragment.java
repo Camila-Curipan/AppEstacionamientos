b70d1d9896aba322214e13e17e4fa10539ba547a
Camila-Curipan <cjcuripan@gmail.com>
2023-12-03 22:57:38 -0300

    metodo cargar lista-registros

--- app/src/main/java/com/example/led/EstacionamientoFragment.java
+++ app/src/main/java/com/example/led/EstacionamientoFragment.java
@@ -1,11 +1,20 @@
 package com.example.led;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class EstacionamientoFragment extends Fragment {
     /* Yo supongo que es aca donde se muestran solo los estacionamientos disponibles y
     se selecciona el que deseamos que nos arroje lecturas de distancia
      */
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Infla el diseño de tu fragmento
+        View rootView = inflater.inflate(R.layout.activity_estacionamientos, container, false);
+        return rootView;
+    }
 
 
 }
